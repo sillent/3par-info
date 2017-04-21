ce91b424cd4e1fb71ecf165a3af722b73ff483ae
Dmitry Ulyanov <sillent1987@gmail.com>
2017-04-21 21:24:10 +0300

    added GNU GPL license agreement

--- 3par-info.py
+++ 3par-info.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python
 
+# Copyright (C) 3par-info: Dmitry Ulyanov <sillent1987@gmail.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
 import sys
 try:
     import paramiko
